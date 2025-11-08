e74a53b262bc3ec6f9e78064b40fdde5d063d57a
Marcos Luis Delgado <mluis@tuenti.com>
2025-11-08 12:59:46 +0000

    main: Hero translations

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,24 +1,25 @@
+"use client";
+
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { StoreButtons } from "./store-buttons";
 
 export const Hero = () => {
+  const t = useTranslations("Hero");
+
   return (
     <div className="flex justify-center bg-radial-[at_25%_50%] from-amber-50 via-pink-200 to-amber-50">
       <section className="pt-20 2xl:pt-10 pb-6 px-8 max-w-6xl md:grid md:grid-cols-2 min-h-screen">
         <div className="flex flex-col justify-center text-center items-center md:text-left md:items-start">
           <div className="mt-8 xl:mt-16">
             <h1 className="text-4xl font-bold text-pink-600">
-              Record yourself. Check your form. Track your progress.
+              {t("mainTitle")}
             </h1>
             <h2 className="mt-4 xl:mt-8 font-semibold text-gray-800 text-xl hidden md:block">
-              There is nothing like seeing yourself in action to improve in a
-              sports discipline.
+              {t("subtitle")}
             </h2>
             <p className="mt-6 xl:mt-12 text-gray-600 text-lg">
-              VideoFit is a free workout video recorder, timer and tracker for
-              iOS and Android. Whether you train calisthenics, handstands, yoga,
-              weightlifting or any other discipline, VideoFit will help you
-              improve your form and technique.
+              {t("description")}
             </p>
           </div>
           <div className="mt-12 xl:mt-24">
@@ -29,7 +30,7 @@ export const Hero = () => {
           <Image
             className="max-w-xs"
             src="/app-handstand.png"
-            alt="App screenshot"
+            alt={t("imageAlt")}
             width={320}
             height={630}
             preload
