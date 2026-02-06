da105df70b200481fc8e4489e172954c92357eed
Marcos Luis Delgado <mluis@tuenti.com>
2026-02-06 09:23:11 +0000

    main: Add json-ld

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,7 +3,12 @@ import { Hero } from "@/components/hero";
 import { Locale } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
 
-// TODO: Entering animations for text and stuff. Perhaps writing animation for main phrases.
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  name: "VideoFit",
+  url: "https://www.videofit.app/",
+};
 
 export default async function Home({ params }: PageProps<"/[locale]">) {
   const { locale } = await params;
@@ -11,6 +16,12 @@ export default async function Home({ params }: PageProps<"/[locale]">) {
 
   return (
     <main className="flex flex-col">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
+        }}
+      />
       <Hero />
       <FeatureList />
     </main>
